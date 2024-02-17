345b21d4f99e232e065bdee53021cb72827d2ffa
William Kukah <williamkukah@outlook.com>
2024-02-17 23:28:36 +0000

    First Commit

---
+++ lib/responsive/desktop_body.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class MyDesktopBody extends StatelessWidget {
+  const MyDesktopBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[400],
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: 400,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(6),
+              border: Border(
+                top: BorderSide(
+                  color: Colors.blue,
+                  width: 5
+                )
+              )
+            ),
+            child: Column(
+              children: [
+                Container(
+                  width: 400,
+                  height: 70,
+                  child: Center(
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Susu',
+                            style: TextStyle(
+                              fontSize: 40,
+                            )
+                          ),
+                          TextSpan(
+                            text: 'CRM',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            )
+                          )
+                        ]
+                      )
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 1
+                ),
+                
+                SizedBox(height: 30),
+
+                Text('Please Login'),
+
+                SizedBox(height: 10),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border(right: BorderSide(color: Colors.grey, width: 20)),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Username'
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/responsive/mobile_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyMobileBody extends StatelessWidget {
+  const MyMobileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[300],
+      body: SafeArea(
+        child: Center(
+          
+        )
+      ),
+    );
+  }
+}
