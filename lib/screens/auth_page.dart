1e504332e540bee4b5709a7fe7a1b45749012201
William Kukah <williamkukah@outlook.com>
2024-02-23 03:17:27 +0000

    Create auth_page.dart

---
+++ lib/screens/auth_page.dart
@@ -0,0 +1,24 @@
+import 'package:crm/responsive/desktop_body.dart';
+import 'package:crm/screens/dashboard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            return DashboardPage();
+          } else {
+            return MyDesktopBody();
+          }
+        }),
+      ),
+    );
+  }
+}
