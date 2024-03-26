958ac799edb864b3ca89915198aa652e67e8a065
William Kukah <williamkukah@outlook.com>
2024-03-26 11:47:03 +0000

    Update auth_page.dart

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -15,7 +15,7 @@ class AuthPage extends StatelessWidget {
           if (snapshot.hasData) {
             return const DashboardPage();
           } else {
-            return LoginPage();
+            return const LoginPage();
           }
         }),
       ),
