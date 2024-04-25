ad6b2c5a2e57fb297c996f408fcb9d7d7f0d40dd
William Kukah <williamkukah@outlook.com>
2024-04-25 11:59:31 +0000

    Made changes to these files

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
-import 'package:crm/auth/login_or_register.dart';
+import 'package:crm/screens/login_or_register.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Good Times Susu',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: LoginOrRegister(
-        
-      ),
+      title: 'Good Times Susu Enterprise',
+      home: LoginOrRegisterPage(),
     );
   }
 }
---
+++ lib/screens/login_or_register.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      
+    );
+  }
+}
