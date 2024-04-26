2fa932fe7116938eeabf420e3477a62933c52313
William Kukah <williamkukah@outlook.com>
2024-04-26 02:14:30 +0000

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () { Scaffold.of(context).openDrawer(); },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+      ),
+      backgroundColor: Colors.yellow[200],
+      drawer: const Drawer(
+        backgroundColor: Colors.black87,
+        elevation: 0,
+        shape: LinearBorder(
+          side: BorderSide()
+        ),
+      ),
+    );
   }
 }
