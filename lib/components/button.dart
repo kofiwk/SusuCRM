f6fb3ecba0c0d5112780af33abbf4875482e7239
William Kukah <williamkukah@outlook.com>
2024-04-26 00:38:38 +0000

    Create button.dart

---
+++ lib/components/button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+
+  const MyButton({
+    super.key,
+    required this.text,
+    required this.onTap
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8)
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              fontFamily: 'Urbanist',
+              fontWeight: FontWeight.w100,
+              color: Colors.white
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
