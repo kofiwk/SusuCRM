9b268b94ec06ce931b261f4f3e6cbefde0a05056
William Kukah <williamkukah@outlook.com>
2024-04-27 01:58:37 +0000

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,37 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:crm/components/drawer.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () { Scaffold.of(context).openDrawer(); },
-              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            );
-          },
-        ),
-      ),
+    return Scaffold(
       backgroundColor: Colors.yellow[200],
-      drawer: const Drawer(
-        backgroundColor: Colors.black87,
-        elevation: 0,
-        shape: LinearBorder(
-          side: BorderSide()
-        ),
+      body: Row(
+        children: [
+          MyDrawer(), // Drawer on the left side
+          Expanded(
+            child: Column(
+              children: [
+                AppBar(
+                  elevation: 0,
+                  backgroundColor: Colors.white,
+                  automaticallyImplyLeading: false,
+                  title: const Text(
+                    'Home',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist'
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome',
+                          style: TextStyle(
+                            fontFamily: 'Urbanist',
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
